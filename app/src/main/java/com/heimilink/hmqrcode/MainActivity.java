81eb26d78604af9920024f5e238d07629c877d7d
gaopeng <316393266@qq.com>
2017-06-16 17:36:42 +0800

    change code can input qrcode content  and change app theme

--- app/src/main/java/com/heimilink/hmqrcode/MainActivity.java
+++ app/src/main/java/com/heimilink/hmqrcode/MainActivity.java
@@ -3,16 +3,14 @@ package com.heimilink.hmqrcode;
 import android.app.Activity;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
-import android.webkit.WebChromeClient;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.google.zxing.BarcodeFormat;
@@ -24,53 +22,38 @@ import com.google.zxing.qrcode.QRCodeWriter;
 import java.util.HashMap;
 import java.util.Map;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements TextWatcher {
     public static final int FLAG_HOMEKEY_DISPATCHED = 0x80000000;
-    private WebView wechatView;
-    private ProgressBar mBar;
+    //一个为二维码信息  一个为请求后台结果地址
+    private EditText qrcodeText, bgUrlText;
     private Button qrBtn;
     private ImageView qrImg;
     private boolean catchHomeKey = false;
+    //接收edittext的值
+    private String content, url;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.getWindow().setFlags(FLAG_HOMEKEY_DISPATCHED, FLAG_HOMEKEY_DISPATCHED);//关键代码
         setContentView(R.layout.activity_main);
-        wechatView = (WebView) findViewById(R.id.login_view);
-        mBar = (ProgressBar) findViewById(R.id.web_progress_bar);
         qrBtn = (Button) findViewById(R.id.qr_btn);
         qrImg = (ImageView) findViewById(R.id.qr_img);
-        WebSettings settings = wechatView.getSettings();
-        settings.setJavaScriptEnabled(true);
-        wechatView.loadUrl("http://www.baidu.com");
-        wechatView.setWebViewClient(new WebViewClient() {
-            @Override
-            public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                Log.d("bopai", "request_url=" + url);
+        qrcodeText = (EditText) findViewById(R.id.qrcode_content);
+        bgUrlText = (EditText) findViewById(R.id.bg_connect_url);
+        qrcodeText.addTextChangedListener(this);
+        bgUrlText.addTextChangedListener(this);
 
-                return super.shouldOverrideUrlLoading(view, url);
-            }
-        });
-        wechatView.setWebChromeClient(new WebChromeClient() {
-            @Override
-            public void onProgressChanged(WebView view, int newProgress) {
-                if (newProgress == 100) {
-                    mBar.setVisibility(View.GONE);
-                } else {
-                    if (View.GONE == mBar.getVisibility()) {
-                        mBar.setVisibility(View.VISIBLE);
-                    }
-                    mBar.setProgress(newProgress);
-                }
-                super.onProgressChanged(view, newProgress);
-            }
-        });
         qrBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Bitmap bitmap = generateBitmap("http://www.baidu.com", 300, 300);
-                qrImg.setImageBitmap(bitmap);
+                if (content != null && !content.isEmpty()) {
+                    Bitmap bitmap = generateBitmap(content, 356, 356);
+                    qrImg.setImageBitmap(bitmap);
+                } else {
+                    //系统默认禁止消息，连同Toast也不能弹出
+                    Toast.makeText(MainActivity.this, "二维码信息为空", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
@@ -124,5 +107,21 @@ public class MainActivity extends Activity {
             Log.e("bopai", "generate_qrcode_error=" + e.getMessage());
         }
         return null;
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+        content = qrcodeText.getText().toString();
+        url = bgUrlText.getText().toString();
     }
 }
